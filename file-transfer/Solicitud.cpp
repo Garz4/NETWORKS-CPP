dad4a81eab5b4f96080ed6989d94a6c58250ef1c
Garz4 <fancygarz4@gmail.com>
2021-09-05 19:57:53 -0500

    Well...

--- file-transfer/Solicitud.cpp
+++ file-transfer/Solicitud.cpp
@@ -20,7 +20,6 @@
 #include "PaqueteDatagrama.h"
 #include "Solicitud.h"
 
-/* TODO(Garz4): Consider making Solicitud constexpr, moving this to header. */
 const Mensaje& Solicitud::envia_y_recibe(
       const std::string& ip, unsigned short puerto, const Mensaje& solicitud) {
   enviar_ = solicitud;
