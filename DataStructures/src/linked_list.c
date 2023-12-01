991e5a54b2debf3bac61a4ecfe0a80ae3d5b7379
Garz4 <fancygarz4@gmail.com>
2023-12-01 11:53:51 -0600

    Update License

--- DataStructures/src/linked_list.c
+++ DataStructures/src/linked_list.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 Uriel Rivas
+ * Copyright (c) 2024 Uriel Rivas
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -15,6 +15,7 @@
  *
  * https://github.com/zoningorg/zoning/blob/main/LICENSE
  */
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
